61aeb703f01740f21cb7a9e7c253c2911c015bef
AadrashKaushal <138370822+AadrashKaushal@users.noreply.github.com>
2023-07-25 18:33:09 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,28 @@
+if(typeof window !== 'undefined'){
+    screenvalue = "";
+let screen =  document.getElementById('screen');
+buttons = document.querySelectorAll('input.button');
+
+for (items of buttons) {
+    items.addEventListener('click' , (e)=>{
+        buttontext = e.target.value;
+        if(buttontext == "Clear"){
+            screenvalue = "";
+            screen.value = screenvalue;
+        }
+        else if( buttontext == "="){
+            screenvalue = eval(screenvalue);
+            screen.value = screenvalue;
+
+         }
+
+         else{
+            screenvalue += buttontext;
+            screen.value = screenvalue;
+         }
+    
+    })
+}
+}
+
+
